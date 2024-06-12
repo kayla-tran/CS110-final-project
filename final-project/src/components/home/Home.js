54160a099e631209bd4ba2ead8b8f5d0e52f1fee
anikkasood <asood008@ucr.edu>
2024-06-11 17:37:21 -0700

    styling stuff

--- final-project/src/components/home/Home.js
+++ final-project/src/components/home/Home.js
@@ -2,33 +2,28 @@ import React, { useEffect, useState } from 'react';
 import './Home.css';
 import chefHat from '../../assets/chefHat.jpeg';
 
+const Home = ({ username }) => {
+  const [posts, setPosts] = useState([]);
 
-  const Home = ({ username }) => {
-    const [posts, setPosts] = useState([]);
-    // const [userName, setUserName] = useState('');
+  const fetchPosts = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/posts');
+      const posts = await response.json();
+      console.log('Response Data:', posts);
+      console.log("Number of posts:", posts.length);
+      setPosts(posts);
+    } catch (err) {
+      console.error('Error fetching posts:', err);
+    }
+  };
 
-    // setUserName(username);
-
-    const fetchPosts = async () => {
-      try {
-        const response = await fetch('http://localhost:8080/posts');
-        const posts = await response.json();
-        console.log('Response Data:', posts);
-        console.log("Number of posts:", posts.length);
-        setPosts(posts);
-      } catch (err) {
-        console.error('Error fetching posts:', err);
-      }
-    };
-    
-  
-    useEffect(() => {
-      fetchPosts();
-     }, []);
-  
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   return (
     <div className="post-container">
+      <p></p>
       {posts.map((post) => (
         <div key={post._id} className="post">
           <div className="element">
@@ -37,30 +32,31 @@ import chefHat from '../../assets/chefHat.jpeg';
                 <img src={chefHat} className="chefHat" alt="Chef Hat" />
               </div>
               <div>
-                <h3>{post.username ?? 'Unknown User'}</h3>
+                <h3>@{post.username ?? 'Unknown User'}</h3>
               </div>
               <div>
                 <p>{new Date(post.createdAt).toLocaleString() ?? 'Unknown Date'}</p>
               </div>
             </div>
           </div>
-          <div className="element">
-            {post.image ? (
-              <img src={post.image} alt="Post" className="post-img" />
-            ) : (
-              <p>No Image Available</p>
-            )}
-          </div>
-          <div className="element">
+          <div className="element post-content"> {/* Move post-content below top-bar */}
+            <div>
+              {post.image ? (
+                <img src={post.image} alt="Post" className="post-img" />
+              ) : (
+                <p>No Image Available</p>
+              )}
+            </div>
             <div className="bottom-box">
-              <p>{post.caption ?? 'No Caption'}</p>
-              <p>{post.content ?? 'No Content'}</p>
+              <p className = "caption">{post.caption ?? 'No Caption'}</p>
+              <p className = "content">{post.content ?? 'No Content'}</p>
             </div>
           </div>
         </div>
       ))}
     </div>
-  );
+);
+
 };
 
 export default Home;
